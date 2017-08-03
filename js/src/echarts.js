ea15939254d582e973279a7a4f623a53a84d9d1d
Youngforzy <352804515@qq.com>
2017-08-03 10:57:44 +0800

    Site updated: 2017-08-03 10:57:43

--- js/src/echarts.js
+++ js/src/echarts.js
@@ -1,34 +1,4 @@
-var txt=document.getElementById("intro").innerHTML;
-document.write(txt);
-var myChart = echarts.init(document.getElementById('echat'));
-var option = {
-title: {
-text: 'ECharts 入门示例'
-},
-tooltip: {},
-legend: {
-data:['销量','统计','11']
-},
-xAxis: {
-data: ["衬衫","羊毛衫","雪纺衫","裤子","高跟鞋","袜子"]
-},
-yAxis: {},
-series: [{
-name: '销量',
-type: 'bar',
-data: [5, 20, 36, 10, 10, 20]
-},{
-name: '统计',
-type: 'bar',
-data: [5, 20, 36, 10, 10, 20]
-},{
-name: '11',
-type: 'line',
-data: [5, 20, 36, 10, 10, 20]
-}]
-};
-  myChart.setOption(option);
-});
+
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
